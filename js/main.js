34044293c5240a2802e5659e6a4eeca7ac3465a9
buronpro <buronbek33@gmail.com>
2022-02-16 22:24:25 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+let elInputSearch = document.querySelector(".films__input-search");
+let elFilmsList = document.querySelector(".films__list");
+let elPrevButton = document.querySelector(".films__prev-button");
+let elNextButton = document.querySelector(".films__next-button");
+let elTemplateFilm = document.querySelector(".film__template").content;
+
+let API_KEY = "6556da72";
+let search = "hulk";
+let page = 1;
+
+function renderMovies(arr, node) {
+  node.innerHTML = null;
+
+  let filmsFragment = document.createDocumentFragment();
+
+  arr.forEach((row) => {
+    let clonedFilmTemplate = elTemplateFilm.cloneNode(true);
+
+    clonedFilmTemplate.querySelector(".film__poster").src = row.Poster;
+    clonedFilmTemplate.querySelector(".film__title").textContent = row.Title;
+    clonedFilmTemplate.querySelector(".film__year").textContent = row.Year;
+    clonedFilmTemplate.querySelector(".film__type").textContent = row.Type;
+    clonedFilmTemplate.querySelector(".film__id").textContent = row.imdbID;
+
+    filmsFragment.appendChild(clonedFilmTemplate);
+  });
+
+  node.appendChild(filmsFragment);
+}
+
+async function getMovies() {
+  let res = await fetch(
+    "https://www.omdbapi.com/?apikey=" +
+      API_KEY +
+      "&s=" +
+      search +
+      "&page=" +
+      page
+  );
+
+  let data = await res.json();
+
+  if (data.Search.length > 0) {
+    renderMovies(data.Search, elFilmsList);
+  }
+
+  if (page === 1) {
+    elPrevButton.disabled = true;
+  } else {
+    elPrevButton.disabled = false;
+  }
+
+  const lastPage = Math.ceil(data.totalResults / 10);
+
+  if (page === lastPage) {
+    elNextButton.disabled = true;
+  } else {
+    elNextButton.disabled = false;
+  }
+}
+
+elInputSearch.addEventListener("input", (evt) => {
+  search = evt.target.value;
+  getMovies();
+});
+
+elPrevButton.addEventListener("click", () => {
+  page--;
+  getMovies();
+});
+elNextButton.addEventListener("click", () => {
+  page++;
+  getMovies();
+});
+simpleslider.getSlider({
+  container: document.getElementById("myslider"),
+  transitionTime: 1,
+  delay: 3.5,
+  prop: "left",
+});
+getMovies();
